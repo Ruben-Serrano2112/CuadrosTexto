0ee0d43aa223dc800a3427f7515124d53c5fbc6e
Ruben <ruben.serrano@iesdoctorbalmis.com>
2021-10-01 12:28:20 +0200

    Agregar archivos de proyecto.

---
+++ CuadrosTexto/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CuadrosTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ayuda_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Ayuda_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Confirmacion_keyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
